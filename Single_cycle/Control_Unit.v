19117417ac0185f20c2582bbb77a7e1caa4330b1
Quoc Bao Nguyen <reusnguyen@outlook.com>
2019-11-09 15:28:37 +0700

    update

--- Single_cycle/Control_Unit.v
+++ Single_cycle/Control_Unit.v
@@ -11,7 +11,7 @@ module Control_Unit(
     output reg [3:0] ALUSel;
     
     // Control Unit output data
-    reg [14:0]data_out; // PCSel_RegWEn_BrUn_BSel_ASel_MemRW_WBSel_ImmSel_ALUSel
+    reg [14:0]data_out; // PCSel_RegWEn_BrUn_BSel_ASel_MemRW_WBSel(2)_ImmSel(3)_ALUSel(4)
 
     // Command classes
     parameter [5:0] R = 5'b01100;
@@ -34,7 +34,7 @@ module Control_Unit(
         AND = 3'b111, ANDI = 3'b111, BGEU = 3'b111;
     
     always@ (*) begin
-       case (inst[6:2])
+        case (inst[6:2])
             R: begin
                 case (inst[14:12])
                     ADD_SUB: begin
@@ -102,8 +102,17 @@ module Control_Unit(
             JAL:    data_out <=
 
             JALR:   data_out <=
-       endcase
-    // Assign value of every single output 
-
+        endcase
+//  Assign value of every single output: 
+//  data_out[14:0] = PCSel_RegWEn_BrUn_BSel_ASel_MemRW_WBSel(2)_ImmSel(3)_ALUSel(4)
+        PCSel <= data_out[14];
+        RegWEn <= data_out[13];
+        BrUn <= data_out[12];
+        BSel <= data_out[11];
+        ASel <= data_out[10];
+        MemRW <= data_out[9];
+        WBSel <= data_out[8:7];
+        ImmSel <= data_out[6:4];
+        ALUSel <= data_out[3:0];
     end
 endmodule 
